5cebe6f3ddc7328a0929ce4de40b985f7b4d5f05
Jan Moxter <jan@moxter.net>
2023-10-24 22:42:49 -0500

    SystemConfig

---
+++ src/main/efaps/ESJP/org/efaps/esjp/promotions/utils/Promotions.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2003 - 2023 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.promotions.utils;
+
+import java.util.UUID;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.api.annotation.EFapsSysConfAttribute;
+import org.efaps.api.annotation.EFapsSystemConfiguration;
+import org.efaps.esjp.admin.common.systemconfiguration.BooleanSysConfAttribute;
+
+@EFapsUUID("73db3d9c-9d5d-49c2-a779-2b8b8e7ae707")
+@EFapsApplication("eFapsApp-Promotions")
+@EFapsSystemConfiguration("e3055a0d-6b3d-4d44-88df-a3042400260b")
+public class Promotions
+{
+    public static final String BASE = "org.efaps.promotions.";
+
+    /** Promotions-Configuration. */
+    public static final UUID SYSCONFUUID = UUID.fromString("e3055a0d-6b3d-4d44-88df-a3042400260b");
+
+    @EFapsSysConfAttribute
+    public static final BooleanSysConfAttribute ACTIVATE = new BooleanSysConfAttribute()
+                    .sysConfUUID(Promotions.SYSCONFUUID)
+                    .key(Promotions.BASE + "Activate")
+                    .description("Activate promotions.");
+}
